913fe7c33cc2f9c0a8fa46b6bda48a9ef67196f7
Daheng Liu <liu@miebach.com>
2022-08-09 17:56:30 -0400

    add second function for inventory data

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import pandas as pd
-from utiles import convert_week_to_date
+from utiles import flow_convert_week_to_date, inventory_convert_week_to_day
 
-week_data = pd.read_excel(
+flow_week_data = pd.read_excel(
     'data/Calculation Example.xlsx', sheet_name='Sheet1')
+inv_week_data = pd.read_excel(
+    'data/Calculation Example.xlsx', sheet_name='Sheet2')
 
-converted_data = convert_week_to_date(week_data)
+converted_data_flow = flow_convert_week_to_date(flow_week_data)
+converted_data_inv = inventory_convert_week_to_day(inv_week_data)
 
 # output converted data to excel file named output.xlsx in data/output folder
-converted_data.to_excel('data/output/output.xlsx', index=False)
+converted_data_flow.to_excel('data/output/flow_output.xlsx', index=False)
+converted_data_inv.to_excel('data/output/inventory_output.xlsx', index=False)
--- utiles.py
+++ utiles.py
@@ -11,7 +11,8 @@ def insert_date_between_week(week_data):
         lambda x: x.strftime('%Y-%m-%d'))
     return week_data
 
-def duplicate_each_row_for_seven_times(week_data):
+
+def duplicate_each_row_for_seven_times(week_data, order_by=['week', 'day']):
     # duplicate each row for seven times
     week_data_origin = week_data.copy()
     # create a blank dataframe
@@ -24,7 +25,7 @@ def duplicate_each_row_for_seven_times(week_data):
             [week_data_duplicate, week_data_origin])
     # sort by week and day
     week_data_duplicate = week_data_duplicate.sort_values(
-        by=['week', 'day'], ascending=True)
+        by=order_by, ascending=True)
 
     return week_data_duplicate
 
@@ -48,9 +49,39 @@ def convert_week_to_date_and_split_weekly_quantity(week_data):
     return week_data
 
 
-def convert_week_to_date(week_data):
+def flow_convert_week_to_date(week_data):
     '''take un processed week data and convert it to date and qty_daily'''
     week_data = duplicate_each_row_for_seven_times(week_data)
     week_data = convert_week_to_date_and_split_weekly_quantity(week_data)
     week_data = add_date_to_each_row(week_data)
     return week_data
+
+
+def convert_week_to_date_and_split_weekly_movement(week_data):
+    # expand week to date and split net_movement to daily_net_qty
+    week_data['date'] = week_data['week'].apply(
+        lambda x: pd.to_datetime(x, format='%V'))
+    week_data['daily_net_qty'] = week_data['net_movement'].apply(
+        lambda x: x / 7)
+    return week_data
+
+
+def inventory_convert_week_to_day(week_data):
+    '''take un processed week data and convert it to date and daily_net_qty'''
+    week_data = duplicate_each_row_for_seven_times(
+        week_data, order_by=['depot', 'week', 'day'])
+    week_data = convert_week_to_date_and_split_weekly_movement(week_data)
+    week_data = add_date_to_each_row(week_data)
+
+    return week_data
+
+
+if __name__ == '__main__':
+    week_data = pd.read_excel(
+        'data/Calculation Example.xlsx', sheet_name='Sheet2')
+    week_data = duplicate_each_row_for_seven_times(
+        week_data, order_by=['depot', 'week', 'day'])
+    week_data = convert_week_to_date_and_split_weekly_movement(week_data)
+    week_data = add_date_to_each_row(week_data)
+
+    print(week_data.head(20))
