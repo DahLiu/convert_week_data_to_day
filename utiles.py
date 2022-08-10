0df2d6023b50571b8a4ede9f865db55947f24305
Daheng Liu <liu@miebach.com>
2022-08-10 15:02:59 -0400

    add columns start inventory and end inventory

--- utiles.py
+++ utiles.py
@@ -1,3 +1,4 @@
+from operator import concat, index
 import pandas as pd
 
 
@@ -72,16 +73,73 @@ def inventory_convert_week_to_day(week_data):
         week_data, order_by=['depot', 'week', 'day'])
     week_data = convert_week_to_date_and_split_weekly_movement(week_data)
     week_data = add_date_to_each_row(week_data)
+    week_data = add_end_inventory_column(week_data)
+    week_data = add_start_inventory_column(week_data)
 
     return week_data
 
 
-if __name__ == '__main__':
-    week_data = pd.read_excel(
-        'data/Calculation Example.xlsx', sheet_name='Sheet2')
-    week_data = duplicate_each_row_for_seven_times(
-        week_data, order_by=['depot', 'week', 'day'])
-    week_data = convert_week_to_date_and_split_weekly_movement(week_data)
-    week_data = add_date_to_each_row(week_data)
+def get_start_inventory(inventory_data):
+    '''get start inventory of each depot'''
+    start_inventory = inventory_data.groupby(['depot'])['starting inv'].first()
+    return start_inventory
 
-    print(week_data.head(20))
+
+def get_date_by_depot(inventory_data, depot):
+    '''get date by depot'''
+    date = inventory_data[inventory_data['depot'] == depot]['date']
+    return date
+
+
+def get_daily_movenment_by_depot(inventory_data, depot):
+    '''get daily movement by depot'''
+    daily_movement = inventory_data[inventory_data['depot']
+                                    == depot]['daily_net_qty']
+    return daily_movement
+
+
+def add_end_inventory_column(inventory_data):
+    '''create list of end inventory by depot'''
+    # get start inventory of each depot
+    start_inventory = get_start_inventory(inventory_data)
+
+    # get list of all depots
+    depot_list = inventory_data['depot'].unique()
+    # get list of dates by depot and get list of daily movement by depot
+    date_list = []
+    daily_movement_list = []
+    for depot in depot_list:
+        date_list.append(get_date_by_depot(inventory_data, depot))
+        daily_movement_list.append(get_daily_movenment_by_depot(
+            inventory_data, depot))
+
+    # create list of end inventory by depot
+    end_inventory_list = []
+    for index, depot in enumerate(depot_list):
+        current_inventory = start_inventory[depot]
+        for i in daily_movement_list[index]:
+            current_inventory += i
+            end_inventory_list.append(current_inventory)
+
+    # convert end inventory to dataframe
+    end_inventory_list = pd.DataFrame(
+        end_inventory_list, columns=['end inventory'])
+    # reset index
+    end_inventory_list = end_inventory_list.reset_index(drop=True)
+    inventory_data = inventory_data.reset_index(drop=True)
+
+    inventory_data = pd.concat([inventory_data, end_inventory_list], axis=1)
+    return inventory_data
+
+
+def add_start_inventory_column(inventory_data):
+    '''add start inventory column to inventory_data'''
+
+    # add start inventory column to inventory_data
+    inventory_data['start inventory'] = inventory_data['end inventory'] - \
+        inventory_data['daily_net_qty']
+    return inventory_data
+
+
+if __name__ == '__main__':
+    print('This is a module. Do not run it directly.')
